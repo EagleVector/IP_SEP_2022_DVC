a30568d340c585045f98bee763cae97deae881b9
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-10-16 16:54:23 +0530

    init for modularity

--- src/housing/__init__.py
+++ src/housing/__init__.py
@@ -1,1 +1,19 @@
-__version__ = "0.0.2"
+import os
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.dir")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath),
+
+    ])
+
+logger = logging.getLogger("deepClassifierLogger")
+
+__version__ = "0.0.3"
